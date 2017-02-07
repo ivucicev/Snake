008a6b0821000625eba900da953f837bd76b0de2
Ivan Vucicevic <ivucicev@gmail.com>
2017-02-07 20:01:07 +0100

    es 6 refactor

--- index.ts
+++ index.ts
@@ -8,11 +8,8 @@ const boardSizeW = boardWidth / blockSize;
 const boardSizeH = boardHeight / blockSize;
 
 let board: Tile[][] = [];
-
 let currentDirection: Key = 37;
-
 let refresh = 250;
-
 let snakeSize: number = 1;
 let snake: ISnakePart[] = [];
 
@@ -21,7 +18,132 @@ interface ISnakePart {
     y: number;
 }
 
-function initBoard() {
+enum Tile {
+    Empty,
+    Snake,
+    Food
+}
+
+enum Key {
+    LEFT = 37,
+    RIGHT = 39,
+    UP = 38,
+    DOWN = 40
+}
+
+
+const drawTile = (x: number, y: number, type: Tile) => {
+    ctx.fillStyle = getTileColor(type);
+    ctx.fillRect(x * blockSize, y * blockSize, blockSize - 1, blockSize - 1);
+}
+
+const draw = () => {
+    
+    clear();
+
+    let tempBoard = board.slice();
+    let nextX = null;
+    let nextY = null;
+
+    // draw board
+    for (let y = 0; y < boardSizeH; y++) {
+        for (let x = 0; x < boardSizeW; x++) {
+            drawTile(x, y, tempBoard[x][y]);
+        }
+    }
+
+    // draw snake
+    for(let s; s < snakeSize; s++) {
+        drawTile(s.x, s.y, Tile.Snake);
+    }
+
+    // SEPARATE SNAKE FROM BOARD, make snake has its own path non dependable of board
+
+}
+
+const move = (x: number, y: number): number[] => {
+    switch(currentDirection) {
+        case Key.DOWN:
+            y++;
+            break;
+        case Key.UP:
+            y--;
+            break;
+        case Key.LEFT:
+            x--;
+            break;
+        case Key.RIGHT:
+            x++;
+            break;
+        default:
+            break;
+    }
+
+    if (x < boardSizeW && y < boardSizeH && y >= 0 && x >= 0) {
+        board[x][y] = Tile.Snake;
+    }
+
+    return [x, y];
+
+}
+
+const clear = () => {
+    ctx.clearRect(0, 0, boardWidth, boardHeight)
+}
+
+const attachListeners = () => {
+    document.onkeyup = bindKeys;   
+}
+
+const bindKeys = (e) => {
+    switch(e.keyCode) {
+        case Key.DOWN:
+            currentDirection = Key.DOWN;
+            break;
+        case Key.UP:
+            currentDirection = Key.UP;
+            break;
+        case Key.LEFT:
+            currentDirection = Key.LEFT;
+            break;
+        case Key.RIGHT:
+            currentDirection = Key.RIGHT;
+            break;
+        default:
+            break;
+    }
+}
+
+const getTileColor = (type: Tile): string => {
+        switch(type) {
+        case Tile.Empty: 
+            return '#9ac503';
+        case Tile.Food:
+            return '#000000';
+        case Tile.Snake:
+            return '#000000';
+        default:
+            return '#9ac503';
+    }
+}
+
+const moveLeft = () => {
+
+}
+
+const moveRight = () => {
+
+}
+
+const moveDown = () => {
+
+}
+
+const moveUp = () => {
+
+}
+
+const initBoard = () => {
 
     for (let y = 0; y < boardSizeH; y++) {
         board[y] = new Array(10);
@@ -31,7 +153,6 @@ function initBoard() {
     }
 
     randomizeStartPositions();
-    console.log(JSON.stringify(board));
     attachListeners();
     setInterval(draw, refresh);
 
@@ -54,144 +175,6 @@ const randomizeStartPositions = () => {
     board[snakeInitY][snakeInitX] = Tile.Snake;
     board[foodInitY][foodInitX] = Tile.Food;
 
-}
-
-function drawTile(x: number, y: number, type: Tile) {
-    ctx.fillStyle = getTileColor(type);
-    ctx.fillRect(x * blockSize, y * blockSize, blockSize - 1, blockSize - 1);
-}
-
-function draw() {
-    
-    clear();
-
-    let tempBoard = board.slice();
-    let nextX = null;
-    let nextY = null;
-
-    //draw snake parts
-
-    // // calculate positions
-    // for (let y = 0; y < boardSizeH; y++) {
-    //     for (let x = 0; x < boardSizeW; x++) {
-    //         if (tempBoard[x][y] == Tile.Snake && x != nextX && y != nextY) {
-    //             board[x][y] = Tile.Empty;
-    //             //break;
-    //             let next = move(x, y);
-    //             nextX = next[0];
-    //             nextY = next[1]; 
-    //             if (board[nextX][nextY] == Tile.Food) {
-    //                 snakeSize++;
-    //                 snake.unshift({
-    //                     x: 1,
-    //                     y: 1,
-    //                 });
-    //             }
-    //             if (board[nextX][nextY] == Tile.Snake) {
-    //                 console.log("GAME OVER");
-    //             }
-    //         }
-    //     }
-    // }
-
-    // draw board
-    for (let y = 0; y < boardSizeH; y++) {
-        for (let x = 0; x < boardSizeW; x++) {
-            drawTile(x, y, tempBoard[x][y]);
-        }
-    }
-
-    // draw snake
-    for(let s; s < snakeSize; s++) {
-        drawTile(s.x, s.y, Tile.Snake);
-    }
-
-    // SEPARATE SNAKE FROM BOARD, make snake has its own path non dependable of board
-
-}
-
-function move(x: number, y: number): number[] {
-    switch(currentDirection) {
-        case Key.DOWN:
-            y++;
-            break;
-        case Key.UP:
-            y--;
-            break;
-        case Key.LEFT:
-            x--;
-            break;
-        case Key.RIGHT:
-            x++;
-            break;
-        default:
-            break;
-    }
-
-    console.log("NEXT", x, y);
-    if (x < boardSizeW && y < boardSizeH && y >= 0 && x >= 0) {
-        board[x][y] = Tile.Snake;
-    }
-
-    return [x, y];
-
-}
-
-function clear() {
-    ctx.clearRect(0, 0, boardWidth, boardHeight)
-}
-
-function attachListeners() {
-    document.onkeyup = bindKeys;   
-}
-
-function bindKeys(e) {
-    switch(e.keyCode) {
-        case Key.DOWN:
-            currentDirection = Key.DOWN;
-            break;
-        case Key.UP:
-            currentDirection = Key.UP;
-            break;
-        case Key.LEFT:
-            currentDirection = Key.LEFT;
-            break;
-        case Key.RIGHT:
-            currentDirection = Key.RIGHT;
-            break;
-        case 32:
-            draw();
-            break;
-        default:
-            break;
-    }
-
-}
-
-function getTileColor(type: Tile): string {
-        switch(type) {
-        case Tile.Empty: 
-            return '#9ac503';
-        case Tile.Food:
-            return '#000000';
-        case Tile.Snake:
-            return '#000000';
-        default:
-            return '#9ac503';
-    }
-}
-
-enum Tile {
-    Empty,
-    Snake,
-    Food
-}
-
-enum Key {
-    LEFT = 37,
-    RIGHT = 39,
-    UP = 38,
-    DOWN = 40
 }
 
 initBoard();
